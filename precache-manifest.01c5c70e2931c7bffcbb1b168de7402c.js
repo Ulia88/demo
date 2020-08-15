c060f2e59bd4d933cac1c699a0926d814106795a
Denys Shutyi <Mufasishe@gmail.com>
2020-08-14 23:37:19 -0700

    Updates

---
+++ precache-manifest.01c5c70e2931c7bffcbb1b168de7402c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7694d8f7aaaf0687f10cc18c369818ae",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "8116e14ce01402ce9f7e",
+    "url": "/demo/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "6958e02e31a8a16159c6",
+    "url": "/demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8116e14ce01402ce9f7e",
+    "url": "/demo/static/js/2.44dd006f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/demo/static/js/2.44dd006f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6958e02e31a8a16159c6",
+    "url": "/demo/static/js/main.05fd63f3.chunk.js"
+  },
+  {
+    "revision": "f0ff667aac3c1b95eeb0",
+    "url": "/demo/static/js/runtime-main.afe8c8ed.js"
+  }
+]);
